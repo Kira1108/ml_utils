b578f953c85c51b8becea03da08777a70f9b32b8
Kira1108 <327485253@163.com>
2022-05-12 11:18:25 +0800

    Initial commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='ml_utils',
-      version='0.0.1',
+      version='0.0.2',
       description='useful function for machine learning',
       author='The fastest man alive.',
       packages=find_packages(),
